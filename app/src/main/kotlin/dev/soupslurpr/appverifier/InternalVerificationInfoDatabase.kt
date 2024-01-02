a21a78458611048dd27c39f733d5710f42f314b8
atilluF <110931720+atilluF@users.noreply.github.com>
2024-01-02 14:54:55 +0100

    Tab fix

--- app/src/main/kotlin/dev/soupslurpr/appverifier/InternalVerificationInfoDatabase.kt
+++ app/src/main/kotlin/dev/soupslurpr/appverifier/InternalVerificationInfoDatabase.kt
@@ -158,7 +158,7 @@ val internalVerificationInfoDatabase = setOf(
             "B3:53:60:1F:6A:1D:5F:D6:60:3A:E2:F5:0B:E8:0C:F3:01:36:7B:86:B6:AB:8B:1F:66:24:3D:A9:6C:D5:73:62",
         )
     ),
-        VerificationInfo(
+    VerificationInfo(
         "app.grapheneos.apps",
         setOf(
             // Official
