a5aff30deb11216c9baa834676f55f8fbd2b6b45
soupslurpr <92235850+soupslurpr@users.noreply.github.com>
2024-01-05 02:10:06 -0800

    version 5/0.5.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "dev.soupslurpr.appverifier"
         minSdk = 28
         targetSdk = 34
-        versionCode = 4
-        versionName = "0.4.0"
+        versionCode = 5
+        versionName = "0.5.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
