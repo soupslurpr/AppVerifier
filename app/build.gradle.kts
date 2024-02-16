c84696bf0711a391df920a8698b1de961c579301
soupslurpr <92235850+soupslurpr@users.noreply.github.com>
2024-02-15 16:15:18 -0800

    Bump Navigation Compose from 2.7.6 to 2.7.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -83,7 +83,7 @@ android {
 dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.activity:activity-compose:1.8.2")
-    implementation("androidx.navigation:navigation-compose:2.7.6")
+    implementation("androidx.navigation:navigation-compose:2.7.7")
     implementation("androidx.datastore:datastore-preferences:1.0.0")
     implementation("androidx.compose.material:material-icons-extended")
     implementation("com.google.android.material:material:1.11.0")
