d1e246c5f811eb9b1f3b80ee8b3b37679151bc83
soupslurpr <92235850+soupslurpr@users.noreply.github.com>
2024-07-12 01:10:28 -0700

    update androidx.datastore:datastore-preferences to 1.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -84,7 +84,7 @@ dependencies {
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.activity:activity-compose:1.9.0")
     implementation("androidx.navigation:navigation-compose:2.7.7")
-    implementation("androidx.datastore:datastore-preferences:1.0.0")
+    implementation("androidx.datastore:datastore-preferences:1.1.1")
     implementation("androidx.compose.material:material-icons-extended")
     implementation("com.google.accompanist:accompanist-drawablepainter:0.33.2-alpha")
 
