b30db4ff3c6dac243767cecfe2067de5fdeb51a8
soupslurpr <92235850+soupslurpr@users.noreply.github.com>
2024-07-12 01:06:34 -0700

    remove dependency on com.google.android.material:material

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -86,7 +86,6 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:2.7.7")
     implementation("androidx.datastore:datastore-preferences:1.0.0")
     implementation("androidx.compose.material:material-icons-extended")
-    implementation("com.google.android.material:material:1.11.0")
     implementation("com.google.accompanist:accompanist-drawablepainter:0.33.2-alpha")
 
     implementation(platform("androidx.compose:compose-bom:2024.02.00"))
