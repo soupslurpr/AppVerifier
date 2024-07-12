7055c060bc88fd5a33c3ca42dff52b9be998a776
soupslurpr <92235850+soupslurpr@users.noreply.github.com>
2024-07-12 01:09:14 -0700

    remove unnecessary comment in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,3 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application") version "8.5.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
