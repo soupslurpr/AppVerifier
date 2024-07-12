5bef4930f1c3515316b18ba5b89e9060cc24f277
soupslurpr <92235850+soupslurpr@users.noreply.github.com>
2024-07-12 01:36:20 -0700

    update Kotlin to 2.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,4 @@
 plugins {
     id("com.android.application") version "8.5.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.0" apply false
 }
