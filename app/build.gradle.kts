5450655ac4c58703e0252df6c804327bd935afd7
soupslurpr <92235850+soupslurpr@users.noreply.github.com>
2024-07-12 01:09:58 -0700

    update androidx.core:core-ktx to 1.13.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -81,7 +81,7 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.activity:activity-compose:1.8.2")
     implementation("androidx.navigation:navigation-compose:2.7.7")
     implementation("androidx.datastore:datastore-preferences:1.0.0")
