73337875730479b8d5114e605893d798e4439a0e
soupslurpr <92235850+soupslurpr@users.noreply.github.com>
2024-07-12 02:19:44 -0700

    add filled List icon

--- app/src/main/kotlin/dev/soupslurpr/appverifier/ui/StartupScreen.kt
+++ app/src/main/kotlin/dev/soupslurpr/appverifier/ui/StartupScreen.kt
@@ -19,6 +19,7 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.List
 import androidx.compose.material.icons.filled.FileOpen
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.FilledTonalButton
