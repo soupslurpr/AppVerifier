66e78860bdc2e247cf5f9973a9e3187091b63e41
soupslurpr <92235850+soupslurpr@users.noreply.github.com>
2024-10-09 00:01:10 -0700

    update AndroidX Activity Compose to 1.9.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -76,7 +76,7 @@ android {
 
 dependencies {
     implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.activity:activity-compose:1.9.0")
+    implementation("androidx.activity:activity-compose:1.9.2")
     implementation("androidx.navigation:navigation-compose:2.8.0-beta05")
     implementation("androidx.datastore:datastore-preferences:1.1.1")
     implementation("androidx.compose.material:material-icons-extended")
