bdc5f5d1eb7732bb19c7a69f5adeabefbca7c6be
soupslurpr <92235850+soupslurpr@users.noreply.github.com>
2024-10-18 15:59:27 -0700

    update AndroidX Compose Navigation to 2.8.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -73,8 +73,8 @@ android {
 
 dependencies {
     implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.activity:activity-compose:1.9.2")
-    implementation("androidx.navigation:navigation-compose:2.8.2")
+    implementation("androidx.activity:activity-compose:1.9.3")
+    implementation("androidx.navigation:navigation-compose:2.8.3")
     implementation("androidx.datastore:datastore-preferences:1.1.1")
     implementation("androidx.compose.material:material-icons-extended")
     implementation("com.google.accompanist:accompanist-drawablepainter:0.35.1-alpha")
