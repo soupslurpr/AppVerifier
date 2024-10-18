2f06fadcb46c15dd9cc091aec983f20d2df3817f
soupslurpr <92235850+soupslurpr@users.noreply.github.com>
2024-10-18 16:16:29 -0700

    replace deprecated Icons.Filled.ExitToApp usage with Icons.AutoMirrored.Filled.ExitToApp

--- app/src/main/kotlin/dev/soupslurpr/appverifier/ui/SettingsScreen.kt
+++ app/src/main/kotlin/dev/soupslurpr/appverifier/ui/SettingsScreen.kt
@@ -12,7 +12,7 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.selection.toggleable
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ExitToApp
+import androidx.compose.material.icons.automirrored.filled.ExitToApp
 import androidx.compose.material.icons.filled.Info
 import androidx.compose.material3.Icon
 import androidx.compose.material3.ListItem
@@ -109,7 +109,7 @@ fun SettingsScreen(
                 },
                 icon = {
                     Icon(
-                        imageVector = Icons.Filled.ExitToApp,
+                        imageVector = Icons.AutoMirrored.Filled.ExitToApp,
                         contentDescription = null
                     )
                 }
