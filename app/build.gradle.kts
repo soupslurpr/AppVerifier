5a265dc2127f405b30e8df164a3c1663c58010cd
soupslurpr <92235850+soupslurpr@users.noreply.github.com>
2025-01-12 21:14:47 -0800

    move AndroidX Compose Material Icons Extended to appropriate place

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -76,11 +76,11 @@ dependencies {
     implementation("androidx.activity:activity-compose:1.9.3")
     implementation("androidx.navigation:navigation-compose:2.8.5")
     implementation("androidx.datastore:datastore-preferences:1.1.1")
-    implementation("androidx.compose.material:material-icons-extended")
     implementation("com.google.accompanist:accompanist-drawablepainter:0.37.0")
 
     implementation(platform("androidx.compose:compose-bom:2024.12.01"))
     implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.material:material-icons-extended")
     implementation("androidx.compose.ui:ui-tooling-preview")
     debugImplementation("androidx.compose.ui:ui-tooling")
 }
