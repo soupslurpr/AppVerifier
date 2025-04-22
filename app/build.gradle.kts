e1680aa63194bcf34d6711f4ca786a516e2db783
soupslurpr <92235850+soupslurpr@users.noreply.github.com>
2025-04-22 15:58:22 -0700

    update AndroidX Core KTX to 1.16.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -75,7 +75,7 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.core:core-ktx:1.16.0")
     implementation("androidx.activity:activity-compose:1.10.0")
     implementation("androidx.navigation:navigation-compose:2.8.5")
     implementation("androidx.datastore:datastore-preferences:1.1.2")
