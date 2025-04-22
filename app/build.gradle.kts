26fe8bcd3641b57f4835dec7054835ea085e2fd5
soupslurpr <92235850+soupslurpr@users.noreply.github.com>
2025-04-22 16:02:30 -0700

    update Google Accompanist Drawable Painter to 0.37.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -79,7 +79,7 @@ dependencies {
     implementation("androidx.activity:activity-compose:1.10.1")
     implementation("androidx.navigation:navigation-compose:2.8.9")
     implementation("androidx.datastore:datastore-preferences:1.1.4")
-    implementation("com.google.accompanist:accompanist-drawablepainter:0.37.0")
+    implementation("com.google.accompanist:accompanist-drawablepainter:0.37.2")
 
     implementation(platform("androidx.compose:compose-bom:2025.01.00"))
     implementation("androidx.compose.material3:material3")
